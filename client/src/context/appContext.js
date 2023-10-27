9e2261f29123221659d5b9fa1a6e917079088ac1
Ani <anikaranth47@gmail.com>
2023-10-27 18:53:26 +0530

    made some changes

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -49,7 +49,7 @@ const AppProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
 
     const authFetch = axios.create({
-        baseURL: 'https://pinterest-clone-i8fo.onrender.com/api/v1',
+        baseURL: '<YOUR_BACKEND_URL>',
 
     })
 
